6320fe866f62ae4c2573b84f1f0eed7a24d28174
kimhyoungnam <hnking1004@naver.com>
2018-05-08 17:55:54 +0900

    facebook login

---
+++ app/src/main/java/com/java/bodysignal/LoginActivity.java
@@ -0,0 +1,131 @@
+package com.java.bodysignal;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.firebase.auth.FacebookAuthProvider;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    //LoginButton SigninFacebookButton;
+    //CallbackManager FacebookCallbackManager;
+
+    private CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+
+        super.onCreate(savedInstanceState);
+        //  setContentView(android.R.layout.);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void buttonOnclick(View view){
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+
+
+        LoginManager.getInstance().logInWithPublishPermissions(LoginActivity.this,
+                Arrays.asList("public_profile","email"));
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(final LoginResult result) {
+
+                GraphRequest request;
+                request = GraphRequest.newMeRequest(result.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject user, GraphResponse response) {
+                        if(response.getError() != null){
+
+                        }else {
+                            Log.i("TAG", "user: " + user.toString());
+                            Log.i("TAG", "AccessToken: " + result.getAccessToken().getToken());
+                            setResult(RESULT_OK);
+
+                            Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(i);
+                            finish();
+                        }
+                    }
+                });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender,birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.e("test", "Error: " + error);
+                //finish();
+            }
+
+            @Override
+            public void onCancel() {
+                //finish();
+            }
+        });
+    }
+
+
+
+
+    /*
+    public void onClick(View view) {
+        FacebookCallbackManager = CallbackManager.Factory.create();
+
+
+        SigninFacebookButton = (LoginButton) findViewById(R.id.login_button);
+        SigninFacebookButton.setReadPermissions("email");
+        // If using in a fragment
+
+
+        // Callback registration
+        SigninFacebookButton.registerCallback(FacebookCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        FacebookCallbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+     */
+}
