2983be9b6d31be08649bd197051b5206865bae0a
kimhyoungnam <hnking1004@naver.com>
2018-05-09 17:04:18 +0900

    skeleton structure

---
+++ app/src/main/java/com/java/bodysignal/WorkerRegisterActivity.java
@@ -0,0 +1,12 @@
+package com.java.bodysignal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WorkerRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
