becb0fa51d1a71c4d1cd00bfd78da21c93ccecff
kimhyoungnam <hnking1004@naver.com>
2018-05-24 17:59:55 +0900

    설정 fragment(todo:해당 fragment 기능수정)

---
+++ app/src/main/java/com/java/bodysignal/fragment/AccountModify.java
@@ -0,0 +1,20 @@
+package com.java.bodysignal.fragment;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.java.bodysignal.R;
+
+
+public class AccountModify extends Fragment{
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_accountmodify,container,false);
+    }
+
+}
--- app/src/main/java/com/java/bodysignal/fragment/Setting.java
+++ app/src/main/java/com/java/bodysignal/fragment/Setting.java
@@ -1,20 +1,86 @@
 package com.java.bodysignal.fragment;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.java.bodysignal.R;
+import android.app.Activity;
+
+
+import android.widget.Button;
 
 
 public class Setting extends Fragment {
-
+    Button button1,button2,button3,button4;
     @Nullable
     @Override
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_setting,container,false);
+        View view= inflater.inflate(R.layout.fragment_setting,container,false);
+
+        button1 = (Button)view.findViewById( R.id.button1 );
+        button2 = (Button)view.findViewById( R.id.button2 );
+        button3 = (Button)view.findViewById( R.id.button3 );
+        button4 = (Button)view.findViewById( R.id.button4 );
+
+        button1.setOnClickListener(myListener);
+        button2.setOnClickListener(myListener);
+        button3.setOnClickListener(myListener);
+        button4.setOnClickListener(myListener);
+
+        return view;
+    }
+    View.OnClickListener myListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            button1.setSelected(false);
+            button2.setSelected(false);
+            button3.setSelected(false);
+            button4.setSelected(false);
+
+            switch (v.getId()) {
+                case R.id.button1:
+                    button1.setSelected(true);
+                    moveButton1();
+                    break;
+                case R.id.button2:
+                    button2.setSelected(true);
+                    moveButton2();
+                    break;
+                case R.id.button3:
+                    button3.setSelected(true);
+                    moveButton3();
+                    break;
+                case R.id.button4:
+                    button4.setSelected(true);
+                    moveButton4();
+                    break;
+
+
+            }
+
+        }
+    };
+    public void moveButton1(){
+        getFragmentManager().beginTransaction().replace(R.id.main_frame,new AccountModify()).commit();
+
+    }
+    public void moveButton2(){
+        getFragmentManager().beginTransaction().replace(R.id.main_frame,new WorkerRegister()).commit();
+
+    }
+    public void moveButton3(){
+        getFragmentManager().beginTransaction().replace(R.id.main_frame,new Help()).commit();
+
+    }
+    public void moveButton4(){
+        getFragmentManager().beginTransaction().replace(R.id.main_frame,new Help()).commit();
+
     }
 }
